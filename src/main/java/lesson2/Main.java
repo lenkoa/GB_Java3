c1bc7b3228beb6917c417c9abd2edfdcebe8cf4d
Elena Andryunina <miranel@gmail.com>
2020-12-18 18:54:39 +0300

    init commit for lesson 3.2

---
+++ src/main/java/lesson2/Main.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+import java.sql.Connection;
+
+public class Main {
+
+    private static Connection connection;
+
+    private static void main (String[] args){
+        connection();
+    }
+
+    private static void connection() {
+    }
+}
